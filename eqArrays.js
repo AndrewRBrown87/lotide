4fecff957d1bdb78d60c7ccac21f045dd92a8f30
Andrew Brown <andrew.robert.brown@gmail.com>
2022-10-21 16:50:44 +0000

    Add assertArraysEqual.js

--- eqArrays.js
+++ eqArrays.js
@@ -5,16 +5,16 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   //check if arrays are the same length
-  if(array1.length !== array2.length) {
+  if (array1.length !== array2.length) {
     //return false if arrays are different lengths
     return false;
   }
 
   //loop through and compare each array element
-  for(let i = 0; i < array1.length; i++) {
+  for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       //return false if elements aren't identical
-      return false
+      return false;
     }
   }
 
