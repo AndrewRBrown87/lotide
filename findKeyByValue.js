0d5730bf129cb4d09141d3749909825cadca37e9
Andrew Brown <andrew.robert.brown@gmail.com>
2022-10-24 21:03:34 +0000

    Add findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+// asserEquals Function
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`🙂 Assertion Passed: [${actual}] === [${expected}]`) : console.log(`🤬 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+//input: an object to search for the key with the matching value
+const findKeyByValue = function(input, value) {
+  for (const key of Object.keys(input)) {
+    if (input[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
