711db6d3573b827d89969105a3b6f633bcd76bc3
Andrew Brown <andrew.robert.brown@gmail.com>
2022-10-26 21:50:42 +0000

    map.js

---
+++ map.js
@@ -0,0 +1,44 @@
+//assertArraysEqual Function
+const assertArraysEqual = function(actual, expected) {
+  //compare using the eqArrays function
+  eqArrays(actual, expected) ? console.log(`🙂 Assertion Passed: [${actual}] === [${expected}]`) : console.log(`🤬 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+//eqArrays Function
+const eqArrays = function(array1, array2) {
+  //check if arrays are the same length
+  if (array1.length !== array2.length) {
+    //return false if arrays are different lengths
+    return false;
+  }
+
+  //loop through and compare each array element
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      //return false if elements aren't identical
+      return false;
+    }
+  }
+
+  //return true if array elements turn out to be identical
+  return true;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ["g","c","t","m","t"]);
+
+const results2 = map(words, word => word[word.length-1]);
+assertArraysEqual(results2, ["d","l","o","r","m"]);
+
+const results3 = map(words, word => word[0] + word[word.length-1]);
+assertArraysEqual(results3, ["gd","cl","to","mr","tm"]);
