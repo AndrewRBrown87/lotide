470864f418864a69c8a175bb8af116a70d0f44d3
Andrew Brown <andrew.robert.brown@gmail.com>
2022-10-29 00:22:06 +0000

    Add findKey.js

---
+++ findKey.js
@@ -0,0 +1,25 @@
+// asserEquals Function
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`🙂 Assertion Passed: [${actual}] === [${expected}]`) : console.log(`🤬 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+// findKey Function
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(result1, "noma");
