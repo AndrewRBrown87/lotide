361ab0df19d1e5e5bdbdc27a17c86be519490c48
Andrew Brown <andrew.robert.brown@gmail.com>
2022-11-07 22:40:11 +0000

    Made modular

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,12 @@
+// assertArraysEqual.js
+
+const eqArrays = require('./eqArrays');
+
 //assertArraysEqual Function
 const assertArraysEqual = function(actual, expected) {
   //compare using the eqArrays function
   eqArrays(actual, expected) ? console.log(`🙂 Assertion Passed: [${actual}] === [${expected}]`) : console.log(`🤬 Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
-//eqArrays Function
-const eqArrays = function(array1, array2) {
-  //check if arrays are the same length
-  if (array1.length !== array2.length) {
-    //return false if arrays are different lengths
-    return false;
-  }
-
-  //loop through and compare each array element
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      //return false if elements aren't identical
-      return false;
-    }
-  }
-
-  //return true if array elements turn out to be identical
-  return true;
-};
+module.exports = assertArraysEqual;
 
