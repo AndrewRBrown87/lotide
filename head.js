fc50599d9b76fc791bbc1410c567ed6bcb535fb0
Andrew Brown <andrew.robert.brown@gmail.com>
2022-11-07 22:03:51 +0000

    Made modular

--- head.js
+++ head.js
@@ -1,11 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`🙂 Assertion Passed: [${actual}] === [${expected}]`) : console.log(`🤬 Assertion Failed: [${actual}] !== [${expected}]`);
-};
+// head.js
 
 const head = function(theArray) {
   return theArray[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
